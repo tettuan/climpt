fe858b49155b666f5b0515567c53ffdf324c490a
tettuan <tettuan@gmail.com>
2025-10-06 23:25:04 +0900

    chore: bump version to 1.5.10

--- src/version.ts
+++ src/version.ts
@@ -22,7 +22,7 @@
  * console.log(`Climpt version: ${CLIMPT_VERSION}`);
  * ```
  */
-export const CLIMPT_VERSION = "1.5.9";
+export const CLIMPT_VERSION = "1.5.10";
 
 /**
  * Version of the breakdown package to use.
